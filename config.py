6b4ad7cfe5a247ed53d7ff11eedf0524a63271ec
ainur kholiz <ainur.aprianto@hashmicro.com>
2022-09-12 10:46:59 +0700

    update db

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Configuration(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     # SQLALCHEMY_DATABASE_URI= 'postgresql://postgres:admin''@localhost:5432/flask_sispak'
-    SQLALCHEMY_DATABASE_URI= 'postgresql://wbpwadwqiwqawf:a2df7d4e664fd4426eb826222f5765b14150f1edfa4dc2423f7abfc7d0487992@ec2-44-209-186-51.compute-1.amazonaws.com:5432/du10148hs9eeh'
+    SQLALCHEMY_DATABASE_URI= 'postgresql://ilwhvjgrtpfugr:228c7b3436fe980d4573de38b35c48697ea52df15794a92b62ddbe4f9ad95c90@ec2-54-163-34-107.compute-1.amazonaws.com:5432/d6bm4ppfmigk35'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
